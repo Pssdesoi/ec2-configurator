6702f1daeaa9fd90942c4adf5a252c8aa4a05574
swtpumpkin <swtpumpkin90@gmail.com>
2019-05-22 11:48:38 +0900

    [install.sh] rename nvmNpmPm2.sh to install.sh added set up iptables & installation MongoDB.

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ NVMDIR=~/.nvm
 NVM=~/.nvm/nvm.sh
 NODE_DIR=$NVMDIR/versions/node/$NODE_VERSION
 PM2_PATH=$NODE_DIR/bin/pm2
+MONGO_FILE=/etc/mongod.conf
 
 # Installing NVM
 if [[ ! -d $NVMDIR ]]; then
@@ -33,4 +34,17 @@ if [[ ! -e $PM2_PATH ]]; then
         echo "Installing pm2"
         nvm use $NODE_VERSION
         npm install pm2 -g
+fi
+
+echo "Set up iptables"
+sudo iptables -A PREROUTING -t nat -p tcp --dport 80 -j REDIRECT --to-ports 8080
+sudo iptables -A PREROUTING -t nat -p tcp --dport 443 -j REDIRECT --to-ports 8443
+
+if [[ ! -d $MONGO_FILE ]]; then
+    echo "Installing MongoDB"
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+    echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+    sudo apt-get update
+    sudo apt-get install -y mongodb-org
+    sudo service mongod start
 fi
