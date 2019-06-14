64f9329477b712ebb51deac0d2943cf77eee8db6
swtpumpkin <swtpumpkin90@gmail.com>
2019-06-14 16:14:30 +0900

    Add vim installation

---
+++ install-list/vim.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Install vim
+# ubuntu has unstable vi program
+sudo apt-get update
+sudo apt-get install vim
--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# Installation order nvm > npm > pm2 > iptables > mongoDB
+# Installation order nvm > npm > pm2 > iptables > mongoDB > vim
 . install-list/nvm.sh \
 && . install-list/npm.sh \
 && . install-list/pm2.sh \
 && . install-list/iptables.sh \
-&& . install-list/mongoDB.sh
+&& . install-list/mongoDB.sh \
+&& . install-list/vim.sh
